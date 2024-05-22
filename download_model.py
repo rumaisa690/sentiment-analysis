df4472d81074544e2e1c42f538154f9131ad66d1
Rumaisa Ayoob <90047742+rumaisa690@users.noreply.github.com>
2024-05-21 21:41:08 -0400

    Delete download_model.py

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
-name = 'distilbert-base-uncased-finetuned-sst-2-english'
-model = TFAutoModelForSequenceClassification.from_pretrained(name)
-tokenizer = AutoTokenizer.from_pretrained(name)
-
-# saving model & tokenizer locally
-model.save_pretrained("./model")
-tokenizer.save_pretrained("./model")
